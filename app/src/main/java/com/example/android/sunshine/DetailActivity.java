7002078553cf1674e4ac6f0edc4f8f813f76abca
IS <russland@yandex.ru>
2017-11-27 01:43:13 +0300

    Exercise 4.2 done.

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,27 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
 
+    @SuppressWarnings("FieldCanBeLocal")
+    private TextView weatherForDay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        weatherForDay = findViewById(R.id.weather_for_day);
+        Intent intent = getIntent();
+        if (intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String weather = intent.getStringExtra(Intent.EXTRA_TEXT);
+            weatherForDay.setText(weather);
+        }
     }
+
 }
